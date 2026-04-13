9e2178c5811315a0a9ab676491144901933383c0
kush025priya-spec <kush025priya@gmail.com>
2026-04-13 08:15:33 +0000

    Fixed tests

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -18,3 +18,10 @@ def test_add_numbers():
     out = add_numbers(1, 2)
     expected_out = 3
     assert  out == expected_out, f"Expected {expected_out} but got {out}"
+from pyospackage_kushwaha.example import add_numbers, multiply_numbers
+
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+
+def test_multiply_numbers():
+    assert multiply_numbers(2, 3) == 6
